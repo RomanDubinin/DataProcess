214ca536a681be5d321584d60ce90306e0f36b41
RomanDubinin <roman230394@yandex.ru>
2016-10-04 01:45:13 +0500

    готовое дз

--- hwb2.py
+++ hwb2.py
@@ -10,20 +10,23 @@ while True:
     ret, frame = video_capture.read()
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([100,100,100])
-    upper_blue = np.array([130,255,255])
+    lower_blue = np.array([90,100,50])
+    upper_blue = np.array([140,255,255])
 
     binary = cv2.inRange(hsv_frame, lower_blue, upper_blue)
     
     im2, contours, hierarchy = cv2.findContours(binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    binary = cv2.cvtColor(binary,cv2.COLOR_GRAY2RGB)
 
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
         if (w + h > 50):
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+            cv2.rectangle(binary,(x,y),(x+w,y+h),(0,0,255),2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
+    cv2.imshow('Video2', binary)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
