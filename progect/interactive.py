10640981a6c43fe3f8f92bcbd312576047105e65
RomanDubinin <roman230394@yandex.ru>
2016-10-29 19:04:06 +0500

    произвольные неограниченные передвижения базы

--- progect/interactive.py
+++ progect/interactive.py
@@ -1,6 +1,16 @@
 import cv2
 import sys
 import numpy as np
+import random
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point (self.x + other.x, self.y + other.y)
 
 class MovingProvider():
     def __init__(self, average_frame):
@@ -21,6 +31,12 @@ class MovingProvider():
         return contours
 
 
+def get_random_vector(min_len, max_len):
+    length = random.randint(min_len, max_len)
+    angle  = random.random() * math.pi * 2
+
+    return Point(length * math.cos(angle), length * math.sin(angle))
+
 
 THRESHOLD = 20
 MORPHOLOGIC_OPERATION_SIZE = (2,2)
@@ -31,6 +47,10 @@ video_capture = cv2.VideoCapture(0)
 ret, average_frame = video_capture.read()
 average_frame = cv2.cvtColor(average_frame, cv2.COLOR_BGR2GRAY)
 moving_provider = MovingProvider(average_frame)
+
+HEIGTH, WIDTH = average_frame.shape
+CENTER = Point(WIDTH//2,HEIGTH//2)
+base_pos = CENTER
 
 while True:
     # Capture frame-by-frame
@@ -44,6 +64,8 @@ while True:
         cv2.polylines(frame,cnt,True,(0,255,255))
 
 
+    base_pos = get_random_vector(0, 10) + base_pos
+    cv2.circle(frame, (int(base_pos.x), int(base_pos.y)), 13, (255,0,0), thickness=-1)
     cv2.imshow('Video2', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
